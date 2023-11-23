3774d8f16ea471315b68f5ea7cd708833d6aef66
Brooke Kuhlmann <brooke@alchemists.io>
2023-11-23 15:55:06 -0700

    Added latest build
    Necessary for publishing to NPM.
    
    Milestone: minor

--- build/htmx-remove.js
+++ build/htmx-remove.js
@@ -1,1 +1,1 @@
-"use strict";(function(){htmx.defineExtension("remove",{onEvent:(e,t)=>{if(e==="htmx:afterProcessNode"){const n=t.detail.elt;const o=n.querySelector("[data-remove]");if(o!==null){o.addEventListener("click",()=>{n.remove()})}}}})})();
+"use strict";(function(){htmx.defineExtension("remove",{onEvent:(e,t)=>{const n=t.detail.elt;if(e==="htmx:afterProcessNode"){const o=n.querySelector("[data-remove]");if(o!==null){o.addEventListener("click",e=>{e.preventDefault();n.remove()})}}}})})();
