ac56eca711960f33fb15bfaa26a86ec180683211
Brooke Kuhlmann <brooke@alchemists.io>
2023-11-24 10:26:19 -0700

    Fixed demo feature specs to properly test JavaScript stack
    When originally switching to Capybara/Cuprite for JavaScript testing, I failed to double check Capybara was loading the correct pages and removing associated elements when _clicked_. This corrects that mistake by ensuring JavaScript is being used. Additionally, I've refactored the spec as follows:
    
    * A unique app path is used so we don't collide with the demo application in case both are run at the same time.
    * Static files are dynamically loaded from the app path so there is less maintenance required to maintain this test suite.
    * Redundant specs have been removed so everything is built into the specs that matter.
    
    Milestone: patch

--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -2,39 +2,30 @@
 
 require "rack_helper"
 
-RSpec.describe "Demo", :web do
+RSpec.describe "Demo", :js do
   using Refinements::Pathnames
 
   subject :app do
     Rack::Static.new proc { [200, {"Content-Type" => "text/html"}, ["<h1>Demo</h1>"]] },
                      {
-                       root: Bundler.root.join("tmp/demo"),
-                       urls: %w[
-                         /_button_form.html
-                         /_link_form.html
-                         /htmx-remove.js
-                         /index.html
-                         /stylesheet.css
-                       ]
+                       root: app_path,
+                       urls: app_path.files.map { |path| "/#{path.basename}" }
                      }
   end
 
-  let(:demo_path) { Bundler.root.join "tmp/demo" }
+  let(:app_path) { Bundler.root.join "tmp/test" }
 
   before do
-    demo_path.remove_tree.make_path
-    Bundler.root.join("demo").files.each { |path| path.copy demo_path }
-    Bundler.root.join("lib/htmx-remove.js").copy demo_path
+    app_path.remove_tree.make_path
+    Bundler.root.join("demo").files.each { |path| path.copy app_path }
+    Bundler.root.join("lib/htmx-remove.js").copy app_path
     Capybara.app = app
-  end
-
-  it "displays demonstration page" do
-    visit "/index.html"
-    expect(page).to have_content("Demonstration")
   end
 
   it "adds and removes link", :aggregate_failures do
     visit "/index.html"
+    expect(page).to have_content("Demonstration")
+
     click_link "New Link"
 
     expect(page).to have_content("Cancel Link")
@@ -43,12 +34,14 @@ RSpec.describe "Demo", :web do
     expect(page).not_to have_content("Cancel Link")
   end
 
-  it "adds button", :aggregate_failures do
+  it "adds and removes button", :aggregate_failures do
     visit "/index.html"
-    click_link "New Button"
+    expect(page).to have_content("Demonstration")
 
+    click_link "New Button"
     expect(page).to have_content("Cancel Button")
 
     click_button "Cancel"
+    expect(page).not_to have_content("Cancel Button")
   end
 end
