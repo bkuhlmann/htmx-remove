601e1bce310ba9704d5647672745a92c6b9b3c24
Brooke Kuhlmann <brooke@alchemists.io>
2024-08-23 07:15:08 -0600

    Fixed Rack helper to disable jemalloc for Capybara testing
    Necessary since Chrome doesn't play well with jemalloc (or at least when `LD_PRELOAD` is set).
    
    Milestone: patch

--- spec/rack_helper.rb
+++ spec/rack_helper.rb
@@ -5,6 +5,8 @@ require "capybara/rspec"
 require "rackup/handler/webrick"
 require "spec_helper"
 
+ENV["LD_PRELOAD"] = nil
+
 Capybara.server = :webrick
 Capybara.javascript_driver = :cuprite
 Capybara.save_path = Bundler.root.join "tmp/capybara"
